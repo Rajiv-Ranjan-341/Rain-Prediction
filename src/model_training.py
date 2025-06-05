3ff02dc60ab957ef8c797fbb5b8452ea5e7057dd
Rajiv-Ranjan-341 <rajeevranjan3412@gmail.com>
2025-06-05 05:58:08 +0530

    Update model_training.py
    🌧️ Rain Prediction using Machine Learning
    
    This project predicts whether it will rain today using a machine learning model trained on historical weather data. It uses features like temperature, humidity, pressure, wind speed, and sunshine. A Random Forest Classifier was trained and deployed in an interactive Streamlit web app called WeatherSense Pro. The app provides real-time predictions, probability of rain, visual insights (gauges, charts), and personalized weather tips.

--- src/model_training.py
+++ src/model_training.py
@@ -34,7 +34,7 @@ X = data[selected_features]
 y = data['RainToday']
 
 # Split data
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train,X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Scale features
 scaler = StandardScaler()
